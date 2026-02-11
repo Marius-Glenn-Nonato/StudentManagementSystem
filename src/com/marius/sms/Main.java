b75850556a6a3832c5e56fa031403b68d69d58f0
MARIUS GLENN NONATO <2232731@slu.edu.ph>
2026-02-11 19:39:46 +0800

    Added sample code in Main.java to ensure database connection works

--- src/com/marius/sms/Main.java
+++ src/com/marius/sms/Main.java
@@ -1,7 +1,16 @@
 package com.marius.sms;
 
+import com.marius.sms.backend.dao.RoleDAO;
+import com.marius.sms.backend.entities.Role;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        RoleDAO dao = new RoleDAO();
+        List<Role> roles = dao.getAll();
+        for (Role role : roles) {
+            System.out.println(role);
+        }
     }
 }
